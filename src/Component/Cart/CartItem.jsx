f1ab00bac01dfc757767aefec609937cfd7f685b
zhlhu322 <0322enter@gmail.com>
2025-04-18 01:32:46 +0800

    大小寫修正

--- src/Component/Cart/CartItem.jsx
+++ src/Component/Cart/CartItem.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import QuantityAdjuster from '@/components/ui/quantityAdjuster'
 import CustomCheckbox from '@/components/ui/customCheckbox';
-import DeleteButton from '@/components/ui/DeleteButton';
+import DeleteButton from '@/components/ui/deleteButton';
 
 const CartItem = ({ 
   item, 
