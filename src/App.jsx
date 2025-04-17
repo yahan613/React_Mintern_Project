93481e756fc9e3eea0f259a71ac600b5dfc08ebd
yahan0613 <yahan930718@gmail.com>
2025-04-18 03:38:57 +0800

    add data persistance

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Provider } from "react-redux";
-import { useState } from 'react'
-import './App.css'
+import { PersistGate } from 'redux-persist/integration/react';
+import store, { persistor } from './redux/store';
+
+import './App.css';
 import Home from "./Pages/Home";
 import Product from "./Pages/Product";
 import Faq from "./Pages/Faq";
@@ -9,35 +11,33 @@ import Cart from "./Pages/Cart";
 import Contact from "./Pages/Contact";
 import MemberCenter from "./Pages/Member";
 import Login from "./Pages/Login";
-
 import Navbar from "./Component/Navbar";
-import store from "./redux/store";
 
 import 'animate.css';
-
 
 function App() {
   return (
     <Provider store={store}>
-      <Router
-        className="w-screen">
-        <div
-          style={{ fontFamily: 'Montserrat, sans-serif' }}
-          className="w-screen h-screen m-0 p-0 mt-0 flex flex-col justify-start overflow-x-hidden">
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/product" element={<Product />} />
-            <Route path="/faq" element={<Faq />} />
-            <Route path="/cart" element={<Cart />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/member" element={<MemberCenter />} />
-            <Route path="/login" element={<Login />} />
-          </Routes>
-        </div>
-      </Router>
+      <PersistGate loading={null} persistor={persistor}>
+        <Router>
+          <div
+            style={{ fontFamily: 'Montserrat, sans-serif' }}
+            className="w-screen h-screen m-0 p-0 mt-0 flex flex-col justify-start overflow-x-hidden">
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/product" element={<Product />} />
+              <Route path="/faq" element={<Faq />} />
+              <Route path="/cart" element={<Cart />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route path="/member" element={<MemberCenter />} />
+              <Route path="/login" element={<Login />} />
+            </Routes>
+          </div>
+        </Router>
+      </PersistGate>
     </Provider>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,33 @@
-import { configureStore } from '@reduxjs/toolkit';
-import cartReducer from './cartSlice';
+// store.js
+import { configureStore } from '@reduxjs/toolkit'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // 使用 localStorage 作為儲存位置
+import cartReducer from './cartSlice'
 
+import { combineReducers } from 'redux'
+
+// 把所有 reducer 組合起來（目前只有一個）
+const rootReducer = combineReducers({
+  cart: cartReducer,
+})
+
+// persist 設定
+const persistConfig = {
+  key: 'root',     // 存到 localStorage 的 key 名稱
+  storage,         // 使用 localStorage
+}
+
+// 包裝 reducer
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+// configureStore
 const store = configureStore({
-    reducer: {
-      cart: cartReducer
-    }
-  });
-  
-  export default store;
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false, // 關閉警告（因為 redux-persist 用了一些非序列化的內容）
+    }),
+})
+
+export const persistor = persistStore(store)
+export default store
